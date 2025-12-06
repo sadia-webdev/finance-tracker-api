f4b326b6e6c5579824b3d5a5c64fe573fe9a48ff
sadiya959 <sacdamamoud@gmail.com>
2025-12-06 23:45:01 +0300

    fixed some issued

--- utils/swagger.js
+++ utils/swagger.js
@@ -4,9 +4,9 @@ const options = {
   definition: {
     openapi: "3.0.0",
     info: {
-      title: "Task Manager API",
+      title: "Finance Tracker API",
       version: "1.0.0",
-      description: "API documentation for our task manager backend",
+      description: "API documentation for our finance tracker application",
     },
     servers: [
       {
